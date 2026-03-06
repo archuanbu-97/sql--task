689e7a636fcc07c7f5372a0ddf9cb0d45b9f1ce6
archuanbu-97 <archuanbu1@gmail.com>
2026-03-06 19:42:33 +0530

    Add files via upload

--- SQL TASKS.sql
+++ SQL TASKS.sql
@@ -28,3 +28,61 @@ select*from employees where department="IT" and salary>40000;
 select*from employees where name like "A%";
 SELECT*from employees where salary between 25000 and 30000;
 select*from employees where department="Finance";
+
+
+
+
+use _sql_;
+insert into employees(id ,Name,Age,Department,Salary) values (11,"Arun",27,"IT",45000);
+update employees set salary=50000 where id=3;
+set sql_safe_updates=0;
+update employees set Department="Finance" where Name="Ajay";
+delete from employees where salary<40000;
+delete from employees where id=5;
+
+
+select*from employees;
+select Name,Salary from employees;
+select*from employees where Department="IT";
+select*from employees where Salary>40000;
+select*from employees where Age>25;
+select*from employees where name like "A%";
+select*from employees where salary between 30000 and 50000;
+select*from employees order by salary asc;
+select*from employees order by salary DESC;
+
+
+Alter table employees modify salary DECIMAL(10,2);
+Alter table employees drop column Age;
+
+
+create database tasks;
+use tasks;
+create table workers
+ (id int primary key,name varchar(100),age int,department varchar(100),salary int);
+ use tasks;
+ Alter table workers add column email VARCHAR(100);
+select*from workers;
+insert into workers(id ,Name,Age,Department,Salary) values
+(1,"Arun",28,"HR",30000),
+(2,"Bala",24,"IT",40000),
+(3,"Charan",30,"Finance",50000),
+(4,"Anitha",26,"IT",42000),
+(5,"Divya",23,"HR",28000);
+Alter table workers modify salary decimal(10,2);
+Alter table workers drop Age;
+truncate table workers;
+select*from workers;
+
+
+ 
+
+
+
+
+
+
+
+
+
+
